bce18734c7e6eb05da153dfc91b87e01593a3829
Tomi Javanainen <tomi.javanainen@gmail.com>
2021-03-28 11:54:13 +0300

    Move possible reaction texts to json

--- reaction.py
+++ reaction.py
@@ -1,30 +1,20 @@
+import json
 from time import time
 from random import choice, randint
 
 LAST_REGRET_ALLOWED_INTERVAL = 60  # seconds
 
-TOO_SOON_REACTIONS = [
-    "Eiköhän tämä nyt tullut vähän liian nopeasti. Odottele jonkin aikaa niin katsotaan sitten.",
-    "Kyllä katumukseen kuuluu se, että ei nyt ihan heti rupea samaa tekemään.",
-]
+with open('responses.json') as f:
+    responses = json.load(f)
 
-NEGATIVE_REACTIONS = [
-    "Et kyllä näytä *oikeasti* katuvan. On sellaista seurausten pahoittelua vain tuo.",
-    "Kadut kyllä mutta tämä tapaus on nyt sen verran julkinen että sinusta pitää tehdä esimerkki.",
-    "Syntisi oli teknisesti ottaen hyvin pieni mutta astuit Veli Vanhimman vaimon varpaille, joten erotus napsahtaa.",
-    "Olisihan tässä näitä lieventäviä asianhaaroja mutta lätkäpeli alkaa kohta eikä kiinnosta.",
-]
-
-POSITIVE_REACTIONS = [
-    "Totta kai saat anteeksi, rakas %name%! Katumuksesi on selvästi aitoa ja sinulla on aivan ilmeisesti Jehovan siunaus!",
-    "Saat anteeksi tällä kertaa mutta rajoituksia kyllä tulee.",
-    "Saat anteeksi mutta tästä kyllä ilmoitetaan lavalla että varmasti silti tunnet nahoissasi.",
-    "KV on setäsi kummin kaima joten kaipa me jätetään tämä asia tähän.",
-]
+    TOO_SOON_REACTIONS = responses['too_soon']
+    NEGATIVE_REACTIONS = responses['negative']
+    POSITIVE_REACTIONS = responses['positive']
+    print(f"Responses loaded")
 
 
 def get_reaction(user_name: str, last_regret_timestamp: float) -> str:
-    print(f"In get_reaction - user_name: {user_name}, last_regret_timestamp: {last_regret_timestamp}")
+    # print(f"In get_reaction - user_name: {user_name}, last_regret_timestamp: {last_regret_timestamp}")
     if (time() - last_regret_timestamp) < LAST_REGRET_ALLOWED_INTERVAL:
         reply_table = TOO_SOON_REACTIONS
     elif randint(0, 1) == 0:
