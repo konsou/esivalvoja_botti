4cbe964864dfe29dc3253e7ca83661b55d6d4676
Tomi Javanainen <tomi.javanainen@gmail.com>
2021-04-07 21:24:31 +0300

    More work on Discord integration testing

--- app/tests/test_50_discord.py
+++ app/tests/test_50_discord.py
@@ -3,9 +3,11 @@ import os
 import time
 import asyncio
 import platform
+import subprocess
 
 import discord
 from dotenv import load_dotenv
+from distest import TestCollector, run_dtest_bot
 
 # THIS FIXES AN "Event loop is closed" RuntimeError AFTER EXIT ON WINDOWS
 # The error is a known bug Python/Windows bug:
@@ -37,19 +39,37 @@ class TestDiscordIntegration(unittest.TestCase):
 
         client.run(token)
 
-    # def test_discord_functions(self):
-    #     """
-    #     Test that the bot responds as it should
-    #     """
-    #
-    #     # TODO: THIS
-    #     bot_token = os.getenv('DISCORD_TOKEN_DEVELOPMENT')
-    #     bot_client = discord.Client()
-    #     test_client_token = os.getenv('DISCORD_TEST_CLIENT_TOKEN')
-    #     test_client_client = discord.Client()
-    #
-    #
-    #     # bot_client.run(bot_token)
-    #     # test
+    def test_discord_functions(self):
+        """
+        Test that the bot responds as it should
+        """
+
+        # TODO: THIS
+        tester_client_token = os.getenv('DISCORD_TEST_CLIENT_TOKEN')
+        test_channel_id = os.getenv('DISCORD_TEST_CHANNEL_ID')
+        tester_client = discord.Client()
+
+        test_collector = TestCollector()
+
+        @test_collector()
+        async def test_silence(interface):
+            """
+            Don't reply if not mentioned
+            """
+            await interface.send_message("Random comment")
+            await interface.ensure_silence()
+
+        # Run the main bot as a non-blocking process
+        subprocess.Popen(["python3", "main.py"])
+        time.sleep(5)
+        run_dtest_bot([os.getenv('DISCORD_DEV_BOT_ID'),
+                       os.getenv('DISCORD_TEST_CLIENT_TOKEN'),
+                       "-c",
+                       os.getenv('DISCORD_TEST_CHANNEL_ID'),
+                       "-r",
+                       "all"],
+                      test_collector)
+
+        # test
 
 
