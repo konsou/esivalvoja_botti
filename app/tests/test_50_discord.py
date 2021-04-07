aff55caf55352d1aaa4f0369f0497dbdbd71fef6
Tomi Javanainen <tomi.javanainen@gmail.com>
2021-04-07 16:42:27 +0300

    Update Discord test

--- app/tests/test_50_discord.py
+++ app/tests/test_50_discord.py
@@ -1,5 +1,3 @@
-# HERE'S A WORKING COMMAND:
-# python .\example_tester.py 825623768763596801 ODI3NTkxMzEyNTYzODk2Mzgw.YGdQeA.NRNK0xmVzmaJNXnMDgLVp8fL5-M -c 688775528831910000 -r all
 import unittest
 import os
 import time
@@ -8,10 +6,10 @@ import asyncio
 import discord
 from dotenv import load_dotenv
 
-# load_dotenv()
+load_dotenv()
 
-'''
-class TestTriggers(unittest.TestCase):
+
+class TestDiscordIntegration(unittest.TestCase):
     def test_can_join_server(self):
         """
         Test that the bot can connect to Discord
@@ -22,28 +20,18 @@ class TestTriggers(unittest.TestCase):
         client = discord.Client()
 
         # THIS WORKS OTHERWISE BUT THROWS AN "Event loop is closed" RuntimeError AFTER EXIT
+        # May be a Windows bug?
 
         @client.event
         async def on_ready():
-            print(f'{client.user} has connected to Discord!')
-            await print_loop()
             await client.close()
-            # await asyncio.sleep(5)
-            await print_loop()
-            # return True
+            return True
 
         @client.event
         async def on_error(err):
-            print(f'Error connecting to Discord: {err}')
-            # return False
-
-        async def print_loop():
-            print(f"loop is {asyncio.get_running_loop()}")
-
+            raise
 
         client.run(token)
-        time.sleep(5)
         print("after")
         print(f"client closed: {client.is_closed()}")
 
-'''
