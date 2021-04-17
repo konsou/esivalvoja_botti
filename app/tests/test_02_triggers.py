271c999ed16aea4c73ca36b2fed0fbf7f182c75e
Tomi Javanainen <tomi.javanainen@gmail.com>
2021-04-17 19:07:46 +0300

    Rearranging things to enable multiple kinds of triggers

--- app/tests/test_02_triggers.py
+++ app/tests/test_02_triggers.py
@@ -10,27 +10,29 @@ class TestTriggers(unittest.TestCase):
         """
         triggers = load_triggers('app/json_data/triggers.json')
         self.assertIsInstance(triggers, dict, 'triggers is a dictionary')
-        self.assertIn('partial', triggers, 'triggers has a key "partial"')
-        self.assertTrue(len(triggers['partial']), 'partial word triggers length more than 0')
+        self.assertIn('regret', triggers, 'triggers has a key "regret"')
+        self.assertIn('partial', triggers['regret'], 'regret triggers has a key "partial"')
+        self.assertTrue(len(triggers['regret']['partial']), 'partial word triggers length more than 0')
 
     def test_trigger_activation(self):
         """
         Test that triggers activate correctly
         """
-        triggers = load_triggers('app/json_data/triggers.json')
-        test_triggers_positive = (triggers['partial'][0],
+        all_triggers = load_triggers('app/json_data/triggers.json')
+        regret_triggers = all_triggers['regret']
+        test_triggers_positive = (regret_triggers['partial'][0],
                                   "kaduttaa",
                                   "kyllä nyt iski katumus :(",
                                   )
         test_triggers_negative = ("well this shouldn't activate",
-                                  f"{triggers['partial'][0][:-1]}!!",
+                                  f"{regret_triggers['partial'][0][:-1]}!!",
                                   )
         for t in test_triggers_positive:
-            self.assertTrue(is_activated(t, triggers),
+            self.assertTrue(is_activated(message=t, triggers=all_triggers),
                             f"trigger should activate: \"{t}\"")
 
         for t in test_triggers_negative:
-            self.assertFalse(is_activated(t, triggers),
+            self.assertFalse(is_activated(message=t, triggers=all_triggers),
                              f"trigger shouldn't activate: \"{t}\"")
 
 
