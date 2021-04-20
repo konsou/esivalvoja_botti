1005f9e7e6df336902740b3a88b8fba3a98be582
Tomi Javanainen <tomi.javanainen@gmail.com>
2021-04-20 22:03:43 +0300

    Tweak type hints

--- app/response.py
+++ app/response.py
@@ -2,10 +2,13 @@ from __future__ import annotations
 import json
 from time import time
 from random import choice, randint
-from typing import Dict, TYPE_CHECKING
+from typing import Dict, List, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from app.options import Options
+
+ResponseList = List[str]
+AllResponses = Dict[str, ResponseList]
 
 
 def load_responses(response_filename: str) -> Dict[str, str]:
@@ -19,7 +22,7 @@ def load_responses(response_filename: str) -> Dict[str, str]:
 def get_response(user_name: str,
                  last_regret_timestamp: float,
                  options: Options,
-                 responses: dict) -> str:
+                 responses: AllResponses) -> str:
     # print(f"In get_response - user_name: {user_name}, last_regret_timestamp: {last_regret_timestamp}")
     # print(f"options: {options}, responses: {responses}")
     if (time() - last_regret_timestamp) < options.last_regret_allowed_interval:
