2a9b84552ec03db63c1ebf5212e7d87109fc07c5
Tomi Javanainen <tomi.javanainen@gmail.com>
2021-04-25 13:31:17 +0300

    load_text_replacements now calculates a hash of the contents

--- app/funnify_text.py
+++ app/funnify_text.py
@@ -1,11 +1,29 @@
 from __future__ import annotations
 import re
 import random
-from typing import TYPE_CHECKING
+import hashlib
+import json
+from typing import Dict, List, TYPE_CHECKING
 
 
 if TYPE_CHECKING:
     from app.options import Options
+
+TextReplacements = Dict[str, List[str]]
+
+
+def load_text_replacements(filename: str,
+                           options: Options) -> TextReplacements:
+    with open(filename, encoding='utf8') as f:
+        string_contents = f.read()
+        text_replacements = json.loads(string_contents)
+        if options.watch_json_data_files:
+            _hash = hashlib.sha256(bytes(string_contents, encoding='utf-8')).hexdigest()
+            print(f"Hash: {_hash}")
+            text_replacements['__hash__'] = [_hash]
+
+    print(f"Text replacements loaded")
+    return text_replacements
 
 
 def funnify_text(text: str,
