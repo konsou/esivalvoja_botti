8bef831e33ce7c7297a46d0ad91e80f0f81f8628
Tomi Javanainen <tomi.javanainen@gmail.com>
2021-04-30 13:06:30 +0300

    Tweak Discord integration tests

--- app/tests_discord/tester_bot.py
+++ app/tests_discord/tester_bot.py
@@ -1,19 +1,31 @@
 # TODO: MORE OF THESE TESTS
+from __future__ import annotations
 import sys
 import os
+from typing import TYPE_CHECKING
 
 from distest import TestCollector, TestInterface
 from distest import run_dtest_bot
 from distest.exceptions import ResponseDidNotMatchError
-
+from discord import Message
 from dotenv import load_dotenv
 
 from app.response import load_responses
 
+if TYPE_CHECKING:
+    from app.response import ResponseList, AllResponses
+
 load_dotenv()
 
 test_collector = TestCollector()
-created_channel = None
+
+
+ALL_RESPONSES = load_responses('app/json_data/responses.json')
+
+# Helper functions
+
+
+# Test functions
 
 
 @test_collector()
@@ -28,19 +40,6 @@ async def test_silence(interface):
 
 
 @test_collector()
-async def test_mention_dont_understand(interface):
-    # TODO: Don't hardcode tester bot ID
-    responses = load_responses('app/json_data/responses.json')
-    await interface.send_message(f"<@{os.getenv('DISCORD_DEV_BOT_ID')}>")
-    response = await interface.wait_for_message()
-    if not any((valid_response in response.content for valid_response in responses['dont_understand'])):
-        error_msg = f"invalid response: {response.content}. Should contain one of: {responses['dont_understand']}"
-        # Raising ResponseDidNotMatchError doesn't print anything - need to print this manually
-        print(error_msg)
-        raise ResponseDidNotMatchError(error_msg)
-
-
-@test_collector()
 async def test_dont_reply_to_everyone_or_here(interface):
     """
     Don't reply to @everyone and @here
@@ -48,6 +47,26 @@ async def test_dont_reply_to_everyone_or_here(interface):
     await interface.send_message(f"@everyone")
     await interface.send_message(f"@here")
     await interface.ensure_silence()
+
+
+@test_collector()
+async def test_mention_dont_understand(interface):
+    valid_responses = ALL_RESPONSES['dont_understand']
+
+    def check_message_valid(message: Message) -> bool:
+        result = any((valid_response in message.content for valid_response in valid_responses))
+        if not result:
+            print(f"invalid response: {message.content}. Should contain one of: {valid_responses}")
+        return result
+
+    await interface.send_message(f"<@{os.getenv('DISCORD_DEV_BOT_ID')}>")
+    return await interface.wait_for_event("message",
+                                          check=check_message_valid,
+                                          timeout=5)
+
+    # response = await interface.wait_for_message()
+    # if not any((valid_response in response.content for valid_response in ALL_RESPONSES['dont_understand'])):
+
 
 
 if __name__ == "__main__":
