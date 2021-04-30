50721bdb33dc267caf712cb20c1d643f7910064f
Tomi Javanainen <tomi.javanainen@gmail.com>
2021-04-30 22:12:39 +0300

    test_mention_regret now working

--- app/tests_discord/tester_bot.py
+++ app/tests_discord/tester_bot.py
@@ -7,7 +7,7 @@ from typing import TYPE_CHECKING
 from distest import TestCollector, TestInterface
 from distest import run_dtest_bot
 from distest.exceptions import ResponseDidNotMatchError
-from discord import Message
+from discord import Message, User
 from dotenv import load_dotenv
 
 from app.response import load_responses
@@ -18,27 +18,6 @@ if TYPE_CHECKING:
 load_dotenv()
 test_collector = TestCollector()
 ALL_RESPONSES = load_responses('app/json_data/responses.json')
-
-
-@test_collector()
-async def test_silence(interface):
-    """
-    Don't reply if not mentioned
-    """
-    await interface.send_message("Esivalvoja tai esivalvoja-testing: mitä kuuluu?")
-    await interface.send_message("Testing for silence")
-    await interface.send_message("Shhhhh...")
-    await interface.ensure_silence()
-
-
-@test_collector()
-async def test_dont_reply_to_everyone_or_here(interface):
-    """
-    Don't reply to @everyone and @here
-    """
-    await interface.send_message(f"@everyone")
-    await interface.send_message(f"@here")
-    await interface.ensure_silence()
 
 
 @test_collector()
@@ -63,19 +42,45 @@ async def test_mention_regret(interface):
     valid_responses.extend(ALL_RESPONSES['positive'])
     valid_responses.extend(ALL_RESPONSES['negative'])
 
-    print(f"username for bot is {interface.client.get_user(os.getenv('DISCORD_DEV_BOT_ID')).display_name}")
+    tester_bot_id: int = int(os.getenv('DISCORD_TEST_BOT_ID'))
+    tester_bot_user: User = interface.client.get_user(tester_bot_id)
 
-    # TODO: %name% replacement
     def check_message_valid(message: Message) -> bool:
-        result = any((valid_response in message.content for valid_response in valid_responses))
-        if not result:
-            print(f"invalid response: {message.content}. Should contain one of: {valid_responses}")
-        return result
+        for valid_response in valid_responses:
+            reponse_with_name = valid_response.replace('%name%', tester_bot_user.display_name)
+            if reponse_with_name in message.content:
+                return True
+
+        # result = any((valid_response.replace('%name%', bot_user.display_name) in message.content for valid_response in valid_responses))
+        
+        print(f"invalid response: {message.content}. Should contain one of: {valid_responses}")
+        return False
 
     await interface.send_message(f"Kadun kovasti! <@{os.getenv('DISCORD_DEV_BOT_ID')}>")
     return await interface.wait_for_event("message",
                                           check=check_message_valid,
                                           timeout=5)
+
+
+@test_collector()
+async def test_silence(interface):
+    """
+    Don't reply if not mentioned
+    """
+    await interface.send_message("Esivalvoja tai esivalvoja-testing: mitä kuuluu?")
+    await interface.send_message("Testing for silence")
+    await interface.send_message("Shhhhh...")
+    await interface.ensure_silence()
+
+
+@test_collector()
+async def test_dont_reply_to_everyone_or_here(interface):
+    """
+    Don't reply to @everyone and @here
+    """
+    await interface.send_message(f"@everyone")
+    await interface.send_message(f"@here")
+    await interface.ensure_silence()
 
 
 if __name__ == "__main__":
